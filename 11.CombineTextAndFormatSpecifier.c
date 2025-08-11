24a2a2c0bea282d08bff18d35cf90498c2f95394
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-11 23:25:26 +0530

    Add files via upload

---
+++ 11.CombineTextAndFormatSpecifier.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main()
+{
+
+ int a = 7;
+ printf("My value is: %d" , a);
+
+int myNum = 15;
+char myLetter = 'D';
+printf("My number is %d and my letter is %c", myNum, myLetter);
+
+
+return 0;
+}
